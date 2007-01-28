351073b9c59fb3ae552239f53bfa6c385899468b
Paul W. Frields <stickster@gmail.com>
2007-01-28 02:10:41 +0000

    One of these comparisons causes an irssi segfault, not good
    git-svn-id: https://irssi-libnotify.googlecode.com/svn/trunk@23 291cbfae-0d27-0410-99d7-557b17e6fe3d

--- notify.pl
+++ notify.pl
@@ -29,9 +29,8 @@ sub notify {
     my $bus = Net::DBus->session;
     return if (!$bus);
     my $svc = $bus->get_service("org.freedesktop.Notifications");
-    return if (!$svc);
     my $obj = $svc->get_object("/org/freedesktop/Notifications");
-    return if (!$obj);
+
     # Make the message entity-safe.  This is all a crappy hack and I'd
     # love to know a Perl-ish way to do this properly.
     $message =~ s/&/&amp;/g;
