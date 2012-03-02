f2f9d7b4485d54b5a824aece18bfe61f90ddb0e6
Paul W. Frields <stickster@gmail.com>
2012-03-02 18:29:43 -0500

    Fix b0rked commit 43ae85..76d58d - inadvertently used old script

--- notify.pl
+++ notify.pl
@@ -8,48 +8,51 @@
 use strict;
 use Irssi;
 use vars qw($VERSION %IRSSI);
-use Net::DBus qw(:typing);
 use HTML::Entities;
 
-
-$VERSION = "0.2.0";
+$VERSION = "0.01";
 %IRSSI = (
     authors     => 'Luke Macken, Paul W. Frields',
     contact     => 'lewk@csh.rit.edu, stickster@gmail.com',
     name        => 'notify.pl',
-    description => 'Use D-Bus to alert user to hilighted messages',
+    description => 'Use libnotify to alert user to hilighted messages',
     license     => 'GNU General Public License',
     url         => 'http://lewk.org/log/code/irssi-notify',
 );
 
-Irssi::settings_add_str('notify', 'notify_icon', 'gtk-dialog-info');
-Irssi::settings_add_str('notify', 'notify_time', '5000');
+Irssi::settings_add_str('notify', 'notify_remote', '');
 
-sub atoi {
-    my $t;
-    foreach my $d (split(//, shift())) {
-	$t = $t * 10 + $d;
-    }
+sub sanitize {
+  my ($text) = @_;
+  encode_entities($text,'\'<>&');
+  my $apos = "&#39;";
+  my $aposenc = "&apos;";
+  $text =~ s/$apos/$aposenc/g;
+  return $text;
 }
 
 sub notify {
     my ($server, $summary, $message) = @_;
-    my $bus = Net::DBus->session;
-    return if (!$bus);
-    my $svc = $bus->get_service("org.freedesktop.Notifications");
-    my $obj = $svc->get_object("/org/freedesktop/Notifications");
 
-    # Make the message entity-safe.
-    encode_entities($message);
+    # Make the message entity-safe
+    $summary = sanitize($summary);
+    $message = sanitize($message);
 
-    $obj->Notify("notify.pl",
-		 0,
-		 Irssi::settings_get_str('notify_icon'),
-		 $summary,
-		 $message,
-		 ['Close', 'Close'],
-		 {0, 0, 0}, 
-		 Irssi::settings_get_str('notify_time'));
+    my $cmd = "EXEC - " .
+	"dbus-send --session /org/irssi/Irssi org.irssi.Irssi.IrssiNotify" .
+	" string:'" . $summary . "'" .
+	" string:'" . $message . "'";
+    $server->command($cmd);
+
+    my $remote = Irssi::settings_get_str('notify_remote');
+    if ($remote ne '') {
+	my $cmd = "EXEC - ssh -q " . $remote .
+	    " \"dbus-send --session /org/irssi/Irssi org.irssi.Irssi.IrssiNotify" .
+	    " string:'" . $summary . "'" .
+	    " string:'" . $message . "'\"";
+	$server->command($cmd);
+    }
+
 }
  
 sub print_text_notify {
@@ -57,25 +60,29 @@ sub print_text_notify {
     my $server = $dest->{server};
 
     return if (!$server || !($dest->{level} & MSGLEVEL_HILIGHT));
-    notify($server, $dest->{target}, $stripped);
+    my $sender = $stripped;
+    $sender =~ s/^\<.([^\>]+)\>.+/\1/ ;
+    $stripped =~ s/^\<.[^\>]+\>.// ;
+    my $summary = $dest->{target} . ": " . $sender;
+    notify($server, $summary, $stripped);
 }
 
 sub message_private_notify {
     my ($server, $msg, $nick, $address) = @_;
 
     return if (!$server);
-    notify($server, "Private message from ".$nick, $msg);
+    notify($server, "PM from ".$nick, $msg);
 }
 
 sub dcc_request_notify {
     my ($dcc, $sendaddr) = @_;
     my $server = $dcc->{server};
 
-    return if (!$server || !$dcc);
+    return if (!$dcc);
     notify($server, "DCC ".$dcc->{type}." request", $dcc->{nick});
 }
 
 Irssi::signal_add('print text', 'print_text_notify');
 Irssi::signal_add('message private', 'message_private_notify');
 Irssi::signal_add('dcc request', 'dcc_request_notify');
-print "D-Bus plugin loaded.";
+
