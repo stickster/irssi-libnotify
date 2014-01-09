8f8f9e115e62140a181e6bee76ca7360b101f4df
Paul W. Frields <stickster@gmail.com>
2014-01-09 11:42:46 -0500

    Clean up shell script

--- irssi-notifier.sh
+++ irssi-notifier.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
 NLPID=`pgrep -u $UID notify-listener`
-DSBA=`cat /proc/$NLPID/environ | tr '\0' '\n' | grep DBUS_SESSION_BUS_ADDRESS | cut -d '=' -f2-`
-DBUS_SESSION_BUS_ADDRESS=$DSBA exec "$@"
+if [ ! -z "$NLPID" ]; then
+    DSBA=`cat /proc/$NLPID/environ | tr '\0' '\n' | grep DBUS_SESSION_BUS_ADDRESS | cut -d '=' -f2-`
+    DBUS_SESSION_BUS_ADDRESS=$DSBA exec "$@"
+fi
