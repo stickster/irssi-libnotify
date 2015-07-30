59985063c97c159d0ff2bf21ee82ef305dfefffe
Max Kovgan <kovganm@gmail.com>
2015-07-30 15:35:52 +0300

    robust irssi-notifier
    - generalize shebang [support not only linux]
    - rely on return code to determine the flow
    - use bashisms to manipulate string vars
    
    Signed-off-by: Max Kovgan <kovganm@gmail.com>

--- irssi-notifier.sh
+++ irssi-notifier.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
-NLPID=`pgrep -u $UID notify-listener`
-if [ ! -z "$NLPID" ]; then
-    DSBA=`cat /proc/$NLPID/environ | tr '\0' '\n' | grep DBUS_SESSION_BUS_ADDRESS | cut -d '=' -f2-`
-    DBUS_SESSION_BUS_ADDRESS=$DSBA exec "$@"
-fi
+#!/usr/bin/env bash
+NLPID=$(pgrep -u "${UID}" notify-listener) || exit 0
+DBUS_SESSION_BUS_ADDRESS_EXPR=$( xargs --null --max-args=1 echo < "/proc/${NLPID}/environ" | grep DBUS_SESSION_BUS_ADDRESS) || exit 0
+DBUS_SESSION_BUS_ADDRESS="${DBUS_SESSION_BUS_ADDRESS_EXPR#*=}" exec "$@"
