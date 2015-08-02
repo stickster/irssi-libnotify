19977be9c1289d6895ae0ca8d1b6acf7b8dfefcd
Max Kovgan <kovganm@gmail.com>
2015-08-02 15:08:53 +0300

    reformat the notifier script
    - comment some bashisms
    - split commands into multiline for further maintenance ease
    
    Signed-off-by: Max Kovgan <kovganm@gmail.com>

--- irssi-notifier.sh
+++ irssi-notifier.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+# get the notify-listener's PID:
 NLPID=$(pgrep -u "${UID}" notify-listener) || exit 0
-DBUS_SESSION_BUS_ADDRESS_EXPR=$( xargs --null --max-args=1 echo < "/proc/${NLPID}/environ" | grep DBUS_SESSION_BUS_ADDRESS) || exit 0
+# take its DBUS_SESSION_BUS_ADDRESS expression:
+DBUS_SESSION_BUS_ADDRESS_EXPR=$(
+	xargs \
+		--null \
+		--max-args=1 \
+		echo < "/proc/${NLPID}/environ" \
+	| grep DBUS_SESSION_BUS_ADDRESS
+) || exit 0
+# set our DBUS_SESSION_BUS_ADDRESS to the one we've found above:
 DBUS_SESSION_BUS_ADDRESS="${DBUS_SESSION_BUS_ADDRESS_EXPR#*=}" exec "$@"
