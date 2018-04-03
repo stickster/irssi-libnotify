73969b0699abb32c59582949404f5bf9dc587def
Gunter Labes <soliton@wesnoth.org>
2018-04-03 23:10:38 +0200

    Fix pgrep finding multiple PIDs
    Also simplified the pipeline to extract DBUS_SESSION_BUS_ADDRESS.

--- irssi-notifier.sh
+++ irssi-notifier.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-NLPID=`pgrep -u $UID notify-listener`
+NLPID=$(pgrep -n -u "$UID" notify-listener)
 if [ ! -z "$NLPID" ]; then
-    DSBA=`cat /proc/$NLPID/environ | tr '\0' '\n' | grep DBUS_SESSION_BUS_ADDRESS | cut -d '=' -f2-`
+    DSBA=$(tr '\0' '\n' < "/proc/$NLPID/environ" | sed -n s/DBUS_SESSION_BUS_ADDRESS=//p)
     DBUS_SESSION_BUS_ADDRESS=$DSBA exec "$@"
 fi
