1d0bb6334d20bf953c74626bef3ce1fe70abfe90
Kyle Russell <bkylerussell@gmail.com>
2018-07-10 14:26:52 -0400

    irssi-notifier.sh: support multiple gnome sessions
    A user may have multiple instances of notify-listener running under
    different gnome sessions, so if pgrep returns multiple processes,
    irssi-notifier.sh dumps error messages in the chat window as a result.
    
    14:09 cat: /proc/2027: Is a directory
    14:09 cat: 3035/environ: No such file or directory

--- irssi-notifier.sh
+++ irssi-notifier.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-NLPID=`pgrep -u $UID notify-listener`
-if [ ! -z "$NLPID" ]; then
-    DSBA=`cat /proc/$NLPID/environ | tr '\0' '\n' | grep DBUS_SESSION_BUS_ADDRESS | cut -d '=' -f2-`
+for pid in `pgrep -u $UID notify-listener`; do
+    DSBA=`cat /proc/$pid/environ | tr '\0' '\n' | grep DBUS_SESSION_BUS_ADDRESS | cut -d '=' -f2-`
     DBUS_SESSION_BUS_ADDRESS=$DSBA exec "$@"
-fi
+done
